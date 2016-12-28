c7a64cfbf3b9f471c6adc3ca4c1b17bb7502f6bc
gaoshusen <gaoshusen520@163.com>
2016-12-28 10:17:34 +0800

    aaa

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+	define('APP_NAME', 'Application');  //项目名称
+	define('APP_PATH', './Application/');  //项目路径
+	define('RUNTIME_PATH', './Application/Temp/'); //临时文件夹路径
+	define('APP_DEBUG', TRUE); //开启调试模式
+
+	include "./TTsys/ThinkPHP.php";  //引用ThinkPHP
+//我是注释
+	?>
